90076b38686be2ddda85084a3b6f0e423d8ce0c6
emcdona0 <emcdonald@chicagobooth.edu>
2014-11-29 11:57:30 -0600

    Added current_user to views/favorites/index

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,10 +1,10 @@
 class Favorite < ActiveRecord::Base
 
   validates(:barber, { :presence => true })
-validates(:barbershop, { :presence => true })
-validates(:user, { :presence => true })
+  validates(:barbershop, { :presence => true })
+  validates(:user, { :presence => true })
 
-belongs_to :user
+  belongs_to :user
   belongs_to :barber
   belongs_to :barbershop
 
